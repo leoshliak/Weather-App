f00c92561e8723631be3f03d441ec864dfb1e389
leoshliak <levshlyak@gmail.com>
2024-05-11 17:43:23 +0300

    measuring system switcher

--- src/index.js
+++ src/index.js
@@ -32,16 +32,31 @@ const hourlyTimes = document.querySelectorAll('.h-time');
 const hLinks = document.querySelectorAll('.h-link');
 const hourlyTemps = document.querySelectorAll('.h-temp');
 const hourlyIcon = document.querySelectorAll('.h-icon');
+const cfBtn = document.querySelector('.cf-btn');
+
+let finalReq = 'kyiv';
 
 async function getCurrentData(){
-    let response = await fetch('http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=kyiv', {mode: 'cors'});
+    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=${finalReq}`, {mode: 'cors'});
+    if(finalReq == 'dnepr' || finalReq == 'dnipro'){
+        response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=dnepropetrovsk`, { mode: 'cors' });
+   }else if(finalReq == 'днепр' || finalReq == 'дніпро'){
+       response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=dnepropetrovsk`, { mode: 'cors' });
+   }
+    
     const currentData = await response.json();
     console.log(currentData);
     let currentDate = currentData.location.localtime.substring(0 , 10);
     let currentTime = currentData.location.localtime.substring(11 , 16);
     const formattedDate = formatDate(currentDate);
     //changeBackground(currentData.current.condition.text);
-    cytyTitle.textContent = currentData.location.name;
+    if( currentData.location.name == 'Dnepropetrovsk'){
+        cytyTitle.textContent = 'Dnipro';
+       }else if(currentData.location.name == 'Днепропетровск' || currentData.location.name == 'Дніпропетровськ'){
+        cytyTitle.textContent = 'Дніпро';
+       }else{
+        cytyTitle.textContent = currentData.location.name;
+       }
     currentTemp.innerHTML = `${currentData.current.temp_c} <span>°C</span>`;
     conditionTeext.textContent = currentData.current.condition.text;
     conditionImg.setAttribute('src', currentData.current.condition.icon);
@@ -50,13 +65,33 @@ async function getCurrentData(){
     feelsLike.textContent = `${currentData.current.feelslike_c} °C`;
     humidity.textContent = `${currentData.current.humidity} %`;
     windSpeed.textContent = `${currentData.current.wind_kph} km/h`;
-   
+
+   function switchCurrentCF(){
+    if(cfBtn.firstChild.classList.contains('active')){
+        currentTemp.innerHTML = `${currentData.current.temp_c} <span>°C</span>`;
+        feelsLike.textContent = `${currentData.current.feelslike_c} °C`;
+        windSpeed.textContent = `${currentData.current.wind_kph} km/h`;
+    }else if(cfBtn.lastChild.classList.contains('active')){
+        currentTemp.innerHTML = `${currentData.current.temp_f} <span>°F</span>`;
+        feelsLike.textContent = `${currentData.current.feelslike_f} °F`;
+        windSpeed.textContent = `${currentData.current.wind_mph} m/h`;
+    }
+  }
+
+    cfBtn.addEventListener('click', switchCurrentCF);
 }
+
+
 
 getCurrentData();
 
  async function  pon(){
-    let response = await fetch(' http://api.weatherapi.com/v1/forecast.json?key=ac6ebdd86e3c4646807110042242304&q=kyiv', {mode: 'cors'});
+    let response = await fetch( `http://api.weatherapi.com/v1/forecast.json?key=ac6ebdd86e3c4646807110042242304&q=${finalReq}`, {mode: 'cors'});
+    if(finalReq == 'dnepr' || finalReq == 'dnipro'){
+        response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=dnepropetrovsk`, { mode: 'cors' });
+   }else if(finalReq == 'днепр' || finalReq == 'дніпро'){
+       response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=dnepropetrovsk`, { mode: 'cors' });
+   }
     const currentData = await response.json();
     console.log(currentData);
     rainChance.textContent = `${currentData.forecast.forecastday[0].day.daily_chance_of_rain} %`;
@@ -136,133 +171,61 @@ getCurrentData();
         element.setAttribute('src', tomorrowData.forecast.forecastday[0].hour[currentHour + index - 24].condition.icon);
        }
   });
+
+
+   function switchCF(){
+     
+    if(cfBtn.firstChild.classList.contains('active')){
+        todayMinTemp.textContent = `${currentData.forecast.forecastday[0].day.mintemp_c} °C`;
+        todayMaxTemp.textContent = `${currentData.forecast.forecastday[0].day.maxtemp_c} °C`;
+    
+        tomorrowMinTemp.textContent = `${tomorrowData.forecast.forecastday[0].day.mintemp_c} °C`;
+        tomorrowMaxTemp.textContent = `${tomorrowData.forecast.forecastday[0].day.maxtemp_c} °C`;
+    
+        afterTomorrowMinTemp.textContent = `${afterTomorrowData.forecast.forecastday[0].day.mintemp_c} °C`;
+        afterTomorrowMaxTemp.textContent = `${afterTomorrowData.forecast.forecastday[0].day.maxtemp_c} °C`;
+
+        hourlyTemps.forEach((element, index) =>{
+            if(currentHour + index <= 23){
+             element.textContent = `${currentData.forecast.forecastday[0].hour[currentHour + index].temp_c} °C`;
+            }else if(currentHour + index > 23){
+                element.textContent = `${tomorrowData.forecast.forecastday[0].hour[currentHour + index - 24].temp_c} °C`;
+            }
+          });
+    }else if(cfBtn.lastChild.classList.contains('active')){
+        todayMinTemp.textContent = `${currentData.forecast.forecastday[0].day.mintemp_f} °F`;
+        todayMaxTemp.textContent = `${currentData.forecast.forecastday[0].day.maxtemp_f} °F`;
+
+        tomorrowMinTemp.textContent = `${tomorrowData.forecast.forecastday[0].day.mintemp_f} °F`;
+        tomorrowMaxTemp.textContent = `${tomorrowData.forecast.forecastday[0].day.maxtemp_f} °F`;
+    
+        afterTomorrowMinTemp.textContent = `${afterTomorrowData.forecast.forecastday[0].day.mintemp_f} °F`;
+        afterTomorrowMaxTemp.textContent = `${afterTomorrowData.forecast.forecastday[0].day.maxtemp_f} °F`;
+
+        hourlyTemps.forEach((element, index) =>{
+            if(currentHour + index <= 23){
+             element.textContent = `${currentData.forecast.forecastday[0].hour[currentHour + index].temp_f} °F`;
+            }else if(currentHour + index > 23){
+                element.textContent = `${tomorrowData.forecast.forecastday[0].hour[currentHour + index - 24].temp_f} °F`;
+            }
+          });
+    }
+  }
+
+  cfBtn.addEventListener('click', switchCF);
  }
+
+ 
 
  pon();
 
 async function searchCurrentData() {
     if (inputSearch.value === '') return;
-       let finalReq = inputSearch.value.toLowerCase();
-    try {
-        const today = new Date();
-        const tomorrow = new Date(today);
-        const afterTomorrow = new Date(today);
-        const thirdDay = new Date(today);
-        
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        afterTomorrow.setDate(afterTomorrow.getDate() + 2);
-        thirdDay.setDate(thirdDay.getDate() + 3);
-    
-         
-        const tomorrowDateString = tomorrow.toISOString().split('T')[0];
-        const afterTomorrowDateString = afterTomorrow.toISOString().split('T')[0];
+        finalReq = inputSearch.value.toLowerCase();
+         getCurrentData();
+        pon();
+        inputSearch.value = '';
        
-    
-        let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=${finalReq}`, { mode: 'cors' });
-        let response2 = await fetch(` http://api.weatherapi.com/v1/forecast.json?key=ac6ebdd86e3c4646807110042242304&q=${finalReq}`, {mode: 'cors'});
-        let response3 = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=ac6ebdd86e3c4646807110042242304&q=${finalReq}&dt=${tomorrowDateString}`, {mode: 'cors'});
-        let response4 = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=ac6ebdd86e3c4646807110042242304&q=${finalReq}&dt=${afterTomorrowDateString}`, {mode: 'cors'});
-        if(finalReq == 'dnepr' || finalReq == 'dnipro'){
-             response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=dnepropetrovsk`, { mode: 'cors' });
-        }else if(finalReq == 'днепр' || finalReq == 'дніпро'){
-            response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ac6ebdd86e3c4646807110042242304&q=dnepropetrovsk`, { mode: 'cors' });
-        }
-        const currentData = await response.json();
-        const currentData2 = await response2.json();
-        const tomorrowData = await response3.json();
-        const afterTomorrowData = await response4.json();
-        console.log(currentData2);
-        let currentHour = parseInt(currentData.location.localtime.substring(11 , 13));
-        if (isNaN(currentHour)) {
-            currentHour = parseInt(currentData.location.localtime.substring(11 , 12));
-        }
-        
-        if (currentData.error) {
-            alert('Location not found.');
-        } else {
-            //changeBackground(currentData.current.condition.text);
-            console.log(currentData);
-            let currentDate = currentData.location.localtime.substring(0 , 10);
-            let currentTime = currentData.location.localtime.substring(11 , 16);
-            const formattedDate = formatDate(currentDate);
-            const tomorrowCalDay = formatDate2(tomorrowData.forecast.forecastday[0].date);
-            const afterTomorrowCalDay = formatDate2(afterTomorrowData.forecast.forecastday[0].date);
-           // img.setAttribute('src', currentData.current.condition.icon);
-           if( currentData.location.name == 'Dnepropetrovsk'){
-            cytyTitle.textContent = 'Dnipro';
-           }else if(currentData.location.name == 'Днепропетровск' || currentData.location.name == 'Дніпропетровськ'){
-            cytyTitle.textContent = 'Дніпро';
-           }else{
-            cytyTitle.textContent = currentData.location.name;
-           }
-           currentTemp.textContent = `${currentData.current.temp_c} °C`;
-           conditionTeext.textContent = currentData.current.condition.text;
-           conditionImg.setAttribute('src', currentData.current.condition.icon);
-           pDate.textContent = formattedDate;
-           pTime.textContent = currentTime;
-           feelsLike.textContent = `${currentData.current.feelslike_c} °C`;
-           humidity.textContent = `${currentData.current.humidity} %`;
-           windSpeed.textContent = `${currentData.current.wind_kph} km/h`;
-           rainChance.textContent = `${currentData2.forecast.forecastday[0].day.daily_chance_of_rain} %`;
-           todayMinTemp.textContent = `${currentData2.forecast.forecastday[0].day.mintemp_c} °C`;
-    todayMaxTemp.textContent = `${currentData2.forecast.forecastday[0].day.maxtemp_c} °C`;
-    todayIcon.setAttribute('src', currentData2.forecast.forecastday[0].day.condition.icon);
-
-    
-    tomorrowDate.textContent = tomorrowCalDay;
-    tomorrowMinTemp.textContent = `${tomorrowData.forecast.forecastday[0].day.mintemp_c} °C`;
-    tomorrowMaxTemp.textContent = `${tomorrowData.forecast.forecastday[0].day.maxtemp_c} °C`;
-    tomorrowIcon.setAttribute('src', tomorrowData.forecast.forecastday[0].day.condition.icon);
-
-
-    
-    afterTomorrowDate.textContent = afterTomorrowCalDay;
-    afterTomorrowMinTemp.textContent = `${afterTomorrowData.forecast.forecastday[0].day.mintemp_c} °C`;
-    afterTomorrowMaxTemp.textContent = `${afterTomorrowData.forecast.forecastday[0].day.maxtemp_c} °C`;
-    afterTomorrowIcon.setAttribute('src', afterTomorrowData.forecast.forecastday[0].day.condition.icon);
-
-    hourlyTimes.forEach((element, index) =>{
-        if(currentHour + index <= 23){
-            if(currentHour + index < 10){
-                element.textContent = `0${currentHour + index}:00`
-             }else{
-                element.textContent = `${currentHour + index}:00`
-             }
-           
-        } else if(currentHour + index > 23){
-            if(currentHour + index - 24 < 10){
-                element.textContent = `0${currentHour + index - 24}:00`
-             }else{
-                element.textContent = `${currentHour + index - 24}:00`
-             } 
-         } 
-      });
-    
-      hourlyTemps.forEach((element, index) =>{
-        if(currentHour + index <= 23){
-         element.textContent = `${currentData2.forecast.forecastday[0].hour[currentHour + index].temp_c} °C`;
-        }else if(currentHour + index > 23){
-            element.textContent = `${tomorrowData.forecast.forecastday[0].hour[currentHour + index - 24].temp_c} °C`;
-        }
-      });
-    
-      hourlyIcon.forEach((element, index) =>{
-        if(currentHour + index <= 23){
-            element.setAttribute('src', currentData2.forecast.forecastday[0].hour[currentHour + index].condition.icon);
-           }else if(currentHour + index > 23){
-            element.setAttribute('src', tomorrowData.forecast.forecastday[0].hour[currentHour + index - 24].condition.icon);
-           }
-      });
-
-           inputSearch.value = '';
-        }
-    } catch (error) {
-        if (error instanceof TypeError && error.message === "Failed to fetch") {
-            alert('Network error. Please check your internet connection.');
-        } else {
-            console.error(error);
-        }
-    }
 }
 
 /*function changeBackground(condition) {
@@ -330,3 +293,9 @@ switchH.addEventListener('click', (e) =>{
         e.target.classList.add('active');
     }
 });
+
+cfBtn.addEventListener('click', () =>{
+    cfBtn.firstChild.classList.toggle('active');
+        cfBtn.lastChild.classList.toggle('active');
+})
+
